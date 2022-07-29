2fb3976fb496b23d3d4e04047cbbd196704a03d1
kmaglonz <kmaglonz@gmail.com>
2022-07-29 15:46:02 -0700

    Incorporated relevant icon programming from API

--- src/app.js
+++ src/app.js
@@ -91,6 +91,16 @@ function displayCityWeather(response) {
   document.querySelector("#feels-like-temp").innerHTML = Math.round(
     response.data.main.feels_like
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
